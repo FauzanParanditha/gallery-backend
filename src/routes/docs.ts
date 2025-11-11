9aaeb9bf2fb39f264350803fd59ddb9fc6223bd3
Fauzan Paranditha <paranditha@gmail.com>
2025-11-11 10:45:13 +0700

    feat: openapi

--- src/routes/docs.ts
+++ src/routes/docs.ts
@@ -4,4 +4,11 @@ import swaggerUi from "swagger-ui-express";
 
 export const docsRouter = Router();
 const doc = buildOpenApi();
+docsRouter.get("/openapi.json", (req, res) => {
+  res.setHeader("Content-Type", "application/json; charset=utf-8");
+  // (Opsional) Cache 1 menit:
+  res.setHeader("Cache-Control", "public, max-age=60");
+  res.status(200).send(doc);
+});
+
 docsRouter.use("/", swaggerUi.serve, swaggerUi.setup(doc));
